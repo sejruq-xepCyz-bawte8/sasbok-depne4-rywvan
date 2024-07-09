a9eb31a5a6ef35f68ca70b0092024490616e0fc3
counts-cod0m@icloud.com <counts-cod0m@icloud.com>
2024-07-09 09:13:21 +0000

    fff

--- client_code/Forms_Editor/Publish/__init__.py
+++ client_code/Forms_Editor/Publish/__init__.py
@@ -150,6 +150,7 @@ class Publish(PublishTemplate):
   def b_login_click(self, **event_args):
     user = anvil.users.login_with_form()
     if user:
+      self.anvil_email.text = "Успешен вход"
       Notification("Успешен вход :)").show()
     
 
