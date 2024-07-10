298a4232f2a94bbac568e1e737d481b0cdf53004
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-07-10 15:56:06 +0100

    bbb

--- theme/assets/js/viewport.js
+++ theme/assets/js/viewport.js
@@ -13,3 +13,22 @@ if (viewportMetaTag) {
     // Append the new viewport meta tag to the head of the document
     document.head.appendChild(viewportMetaTag);
 }
+
+
+
+history.pushState(null, null, location.href);
+history.back();
+history.forward();
+var count = 0
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+window.addEventListener('popstate', () => {
+  count++;
+  history.go(1);
+
+  if (count % 2 === 1 && (isSafari || count > 2)) { // In other browser, the first time user press back value of count is > 2, but safari is not
+    
+    const container = document.getElementById('navigation');
+    const firstElement = container.querySelector(':first-child');
+    anvil.call($("#appGoesHere > div"), "open_form", $(firstElement))
+  }
+});
