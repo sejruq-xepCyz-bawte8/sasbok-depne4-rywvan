038229fda9174442c86e7a373b174c9fbd057f9c
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-07-10 04:15:42 +0100

    dd

--- client_code/App/Browser.py
+++ client_code/App/Browser.py
@@ -1,10 +1,3 @@
-import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-import anvil.users
 from anvil.js import window
 
 #"loadingSpinner"
@@ -16,5 +9,7 @@ class BrowserClass:
         self.protocol = window.location.protocol
         self.host = window.location.host
         self.hostname = window.location.hostname
+        if 'maxTouchPoints' in window.navigator:
+            print(window.navigator.maxTouchPoints)
 
   
