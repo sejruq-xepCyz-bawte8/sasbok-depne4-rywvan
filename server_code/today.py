5bf336e0ad75de93577054bd27acd16d6c4a1cbd
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-07-18 17:21:04 +0100

    ttt

---
+++ server_code/today.py
@@ -0,0 +1,30 @@
+import anvil.server
+import anvil.http
+import json
+        
+
+@anvil.server.background_task
+def make_today_request():
+  response = request_home()
+  print(response)
+        
+def request_home(api:str, url:str, data:dict=None, info:str=None):
+        headers:dict = {
+        'Cheteme':'get_home',
+    }
+        url1 = f'https://api.chete.me/chart-home-age-1'
+        url0 = f'https://api.chete.me/chart-home-age-0'
+        try:
+                response = anvil.http.request(
+                                        url=url1,
+                                        headers = headers,
+                                        method='GET',
+                                        json=True
+                                        )
+                status = 200
+        except anvil.http.HttpError as e:
+                response = None
+                status = e.status
+        return response
+    
+       
