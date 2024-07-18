1e4a9f10525966bfa1f373b92bf66b45e08e2447
counts-cod0m@icloud.com <counts-cod0m@icloud.com>
2024-07-18 16:44:18 +0000

    Edited settings and today

--- server_code/today.py
+++ server_code/today.py
@@ -1,23 +1,49 @@
 import anvil.server
 import anvil.http
+from anvil.tables import app_tables
 import json
+
         
+
+@anvil.server.callable
+def get_home_chart(age):
+  row = app_tables.charts.get(api='get_home', age=age)
+  return row
+
 
 @anvil.server.background_task
 def make_today_request():
-  response = request_home()
-  print(response)
+  response = request_home(1)
+  if response:
+    save_chat(1, response)
+  else:
+    print('no resp 1')
+    
+  response = request_home(0)
+  if response:
+    save_chat(0, response)
+  else:
+    print('no resp 0')
+
+
+def save_chat(age, response):
+    row = app_tables.charts.get(api='get_home', age=age)
+    if row:
+      row['response'] = response
+    else:
+      app_tables.charts.add_row(age=age, api='get_home', response=response)
+
         
-def request_home():
+def request_home(age):
         headers:dict = {
         'Cheteme':'get_home',
-        'Cheteme-Age':1
+        'Cheteme-Age':age
     }
-        url1 = f'https://api.chete.me/chart-home-age-1'
-        url0 = f'https://api.chete.me/chart-home-age-0'
+        url = f'https://api.chete.me/chart-home-age-{age}'
+
         try:
                 response = anvil.http.request(
-                                        url=url1,
+                                        url=url,
                                         headers = headers,
                                         method='GET',
                                         json=True
