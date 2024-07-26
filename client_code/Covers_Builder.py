b3fef8607629a0cd4f01f30a45d557a2c13c73ed
counts-cod0m@icloud.com <counts-cod0m@icloud.com>
2024-07-26 10:00:38 +0000

    Edited Covers_Builder

--- client_code/Covers_Builder.py
+++ client_code/Covers_Builder.py
@@ -5,7 +5,7 @@ from anvil.js.window import jQuery as jQ
 def fill_panel(panel_id, work_ids:list=None, works:list=None):
     panel = jQ(f'#{panel_id}')
     panel.html('')
-    if work_id:
+    if work_ids:
       for work_id in work_ids:
         cover = WORKS.make_cover(work_id=work_id)
         panel.append(cover)
